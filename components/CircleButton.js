c889739c26c565c439816aa0fe3cdf0213c4c68e
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-07-09 22:51:36 -0400

    added CircleButton component

---
+++ components/CircleButton.js
@@ -0,0 +1,33 @@
+import { View, StyleSheet, Pressable } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const CircleButton = ({ onPress }) => {
+  return (
+    <View style={styles.circleButtonContainer}>
+      <Pressable style={styles.CircleButton} onPress={onPress}>
+        <MaterialIcons name="add" size={38} color="#25292e" />
+      </Pressable>
+    </View>
+  );
+};
+
+export default CircleButton;
+
+const styles = StyleSheet.create({
+  circleButtonContainer: {
+    width: 84,
+    height: 84,
+    marginHorizontal: 60,
+    borderWidth: 4,
+    borderColor: "#ffd33d",
+    borderRadius: 42,
+    padding: 3,
+  },
+  CircleButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 42,
+    backgroundColor: "#fff",
+  },
+});
