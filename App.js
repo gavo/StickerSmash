fa9c90c71f23a554a5195fa65ea51e4dc21f8f24
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-07-11 21:15:57 -0400

    Adding EmojiSticker to the app

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import IconButton from "./components/IconButton";
 import CircleButton from "./components/CircleButton";
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from "./components/EmojiList";
+import EmojiSticker from "./components/EmojiSticker";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
@@ -54,6 +55,9 @@ export default function App() {
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
       </View>
 
       {showAppOptions ? (
